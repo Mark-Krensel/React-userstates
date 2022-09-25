d9ec9afa66f3e53409caf39e0577ab131d968077
Mark Krensel <mark.krensel@gmail.com>
2022-09-25 12:29:38 +0200

    add functionality to switch

--- src/components/ActiveToggle/ActiveToggle.js
+++ src/components/ActiveToggle/ActiveToggle.js
@@ -1,10 +1,17 @@
 import "./ActiveToggle.css";
+import { useState } from "react";
 
 function ActiveToggle() {
+  const [state, setState] = useState(false);
   return (
     <main>
-      <div className="box" />
-      <button type="button">Activate</button>
+      <div className={`box  ${state === true && "box--active"}`} />
+      <button
+        type="button"
+        onClick={() => setState((previousState) => !previousState)}
+      >
+        {state ? "Deactivate" : "Activate"}
+      </button>
     </main>
   );
 }
